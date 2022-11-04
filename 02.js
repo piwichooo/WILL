3a6db2e04acfd42c4dbe9ff4add76326c6d0c0d9
piwichooo <brian.net2019@gmail.com>
2022-11-03 20:23:58 -0500

    Segundo problema resuelto

--- 02.js
+++ 02.js
@@ -12,6 +12,9 @@ function stringMasLarga(strings) {
   // stringMasLarga(['JavaScript', 'HTML', 'CSS']); debe retornar 'JavaScript'
 
   // Tu código aca
+  return strings.reduce((acc, val) => {
+    return (acc.length > val.length) ? acc : val;
+}, '')
 }
 
 // No modifiques nada debajo de esta linea //
